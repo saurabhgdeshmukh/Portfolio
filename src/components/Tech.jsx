d2ff6afe965cf559e658f906d87c22511068a6cb
saurabhgdeshmukh <saurabhgdeshmukh@gmail.com>
2025-01-11 23:24:12 +0530

    update

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import const [first, setfirst] = useState(second)
 import { BallCanvas } from "./canvas";
 import  {SectionWrapper}  from "../hoc";
 import { technologies } from "../constants";
